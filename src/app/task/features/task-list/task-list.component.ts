74df0dd1018ad103ebdd14833f58fd95d4763b06
Patricio Avila <p.avil;af1998@gmail.com>
2024-11-05 17:47:19 -0300

    Adding the Button to eliminated Tasks.. and logig to make it work

--- src/app/task/features/task-list/task-list.component.ts
+++ src/app/task/features/task-list/task-list.component.ts
@@ -1,10 +1,9 @@
-import { Component, inject, OnInit} from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { TableComponent } from '../../ui/table/table.component';
 import { RouterLink } from '@angular/router';
 import { Task, TaskService } from '../../data-access/task.service';
 import { BehaviorSubject, catchError, finalize, Observable, of, startWith, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-task-list',
@@ -18,23 +17,34 @@ export default class TaskListComponent implements OnInit {
   loading$: Observable<boolean>;
 
   constructor(private taskService: TaskService) {
-    // Inicializa loading$ a true antes de la carga
     this.loading$ = of(true);
   }
 
   ngOnInit() {
-    // Cambia loading$ a true y luego asigna las tareas
     this.loading$ = of(true);
     this.tasks$ = this.taskService.getTasks().pipe(
-      startWith([]), // Permite que loading$ se mantenga como verdadero mientras carga
+      startWith([]),
       catchError((error) => {
         console.error('Error al cargar tareas:', error);
-        return of([]); // Maneja el error devolviendo un array vacío
+        return of([]);
       }),
       tap(() => {
-        // Establece loading$ a false después de que se cargan las tareas
         this.loading$ = of(false);
       })
     );
   }
+
+  // Función para eliminar una tarea
+  deleteTask = (id: string) => {
+    if (confirm('¿Estás seguro de que deseas eliminar esta tarea?')) {
+      this.taskService.deleteTask(id).then(() => {
+        alert('Tarea eliminada con éxito');
+        // Refresca la lista de tareas
+        this.ngOnInit();
+      }).catch(error => {
+        console.error('Error eliminando la tarea:', error);
+      });
+    }
+  }
+  
 }
--- src/app/task/ui/table/table.component.ts
+++ src/app/task/ui/table/table.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Task } from '../../data-access/task.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { AngularFirestore  } from '@angular/fire/compat/firestore';
+
 @Component({
   selector: 'app-table',
   standalone: true,
@@ -10,11 +10,7 @@ import { AngularFirestore  } from '@angular/fire/compat/firestore';
   templateUrl: './table.component.html',
   styles: []
 })
-
-
 export class TableComponent {
   @Input() tasks: Task[] = [];
-
-
- 
+  @Input() deleteTask!: (taskId: string) => void;
 }
